56f2e3c4874fc75d38e4492b1abde3884ede6b91
hssm <houssam.salem.au@gmail.com>
2024-12-22 19:01:38 +1100

    Move module to top level
    We're not gaining anything with the sub-module

--- __init__.py
+++ __init__.py
@@ -1,1 +1,96 @@
-from . import mcne
+import json
+
+from aqt import *
+from aqt.editor import Editor
+from aqt.webview import WebContent
+
+addon_package = mw.addonManager.addon_from_module(__name__)
+
+class MCNE:
+    def __init__(self):
+        self.cc_spin = None
+        self.editor = None
+        self.config = None
+
+    def editor_init(self, editor):
+        self.editor = editor
+        self.cc_spin = QSpinBox(editor.widget)
+        hbox = QHBoxLayout()
+        spacer = QLabel("", editor.widget)
+        label = QLabel("Columns:", editor.widget)
+        hbox.addWidget(spacer)
+        hbox.addStretch()
+        hbox.addWidget(label)
+        hbox.addWidget(self.cc_spin)
+
+        self.cc_spin.setMinimum(1)
+        self.cc_spin.setMaximum(18)
+        self.cc_spin.valueChanged.connect(
+            lambda count: self.on_column_count_changed(count)
+        )
+        editor.outerLayout.addLayout(hbox)
+
+    def save_config(self, note_config):
+        config = mw.col.get_config('mcne', dict())
+        mid = str(self.editor.note.mid)
+        config[mid] = note_config
+        mw.col.set_config('mcne', config)
+
+    def get_note_config(self):
+        config = mw.col.get_config('mcne', dict())
+        note_config = config.get(str(self.editor.note.mid), None)
+        # Model has no config. Build one
+        if not note_config:
+            note_config = {
+                'column_count': 1,
+                'field_sizes': []
+            }
+        # Ensure there is an index for each field in the note
+        # (either first visit or note has been modified)
+        missing = len(self.editor.note.fields) - len(note_config['field_sizes'])
+        for m in range(0, missing):
+            note_config['field_sizes'].append(1)
+
+        return note_config
+
+    def on_webview_will_set_content(self, web_content: WebContent, context):
+        if not isinstance(context, Editor):
+            return
+        web_content.js.append(f"/_addons/{addon_package}/web/mcne.js")
+        web_content.css.append(f"/_addons/{addon_package}/web/mcne.css")
+
+    def did_load_note(self, editor, focusTo=None) -> None:
+        note_config = self.get_note_config()
+        self.cc_spin.setValue(note_config['column_count'])
+        self.apply_multicolumn()
+
+    def apply_multicolumn(self):
+        note_config = self.get_note_config()
+        self.editor.web.eval(f"note_config = {json.dumps(note_config)}")
+        self.editor.web.eval(f"apply_multicolumn()")
+
+    def on_column_count_changed(self, count):
+        note_config = self.get_note_config()
+        note_config['column_count'] = count
+        self.save_config(note_config)
+        self.apply_multicolumn()
+
+    def on_js_message(self, handled, message, context):
+        if not message.startswith('MCNE:'):
+            return handled
+
+        payload = json.loads(message[5:])
+        note_config = self.get_note_config()
+        note_config['field_sizes'][int(payload['idx'])] = payload['size']
+        self.save_config(note_config)
+        self.apply_multicolumn()
+        return True, None
+
+
+mw.addonManager.setWebExports(__name__, r"web/.*")
+mcne = MCNE()
+
+gui_hooks.webview_will_set_content.append(mcne.on_webview_will_set_content)
+gui_hooks.editor_did_load_note.append(mcne.did_load_note)
+gui_hooks.editor_did_init.append(mcne.editor_init)
+gui_hooks.webview_did_receive_js_message.append(mcne.on_js_message)
